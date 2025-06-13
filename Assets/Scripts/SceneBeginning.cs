dd14fdef1d45c4500a45e7ee5fe098b7cb943ccf
ChristianDominguezVicente <chris_dovi@hotmail.com>
2025-06-13 21:02:46 +0200

    FinalV2

--- Assets/Scripts/SceneBeginning.cs
+++ Assets/Scripts/SceneBeginning.cs
@@ -6,7 +6,6 @@ public class SceneBeginning : MonoBehaviour
 {
     [SerializeField] private CinematicDialogue cinematicDialogue;
     [SerializeField] private CinematicDialogue cinematicDialogue2;
-    [SerializeField] private CinematicDialogue cinematicDialogue3;
     [SerializeField] private string scene;
     [SerializeField] private bool nextScene = false;
 
@@ -21,7 +20,7 @@ public class SceneBeginning : MonoBehaviour
         {
             SaveSystemMult ssm = FindFirstObjectByType<SaveSystemMult>();
             float karma = PlayerPrefs.GetFloat("Karma", 0);
-            if (karma > 0)
+            if (karma >= 0)
             {
                 cinematicDialogue.PlayDialogue();
 
@@ -30,20 +29,11 @@ public class SceneBeginning : MonoBehaviour
                     yield return null;
                 }
             }
-            else if (karma == 0)
+            else if (karma < 0)
             {
                 cinematicDialogue2.PlayDialogue();
 
                 while (!cinematicDialogue2.End)
-                {
-                    yield return null;
-                }
-            }
-            else if (karma < 0)
-            {
-                cinematicDialogue3.PlayDialogue();
-
-                while (!cinematicDialogue3.End)
                 {
                     yield return null;
                 }
